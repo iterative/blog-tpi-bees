20af3c581f0b235ddcf0c31e95e57f79402aca98
MKhalusova <kafooster@gmail.com>
2021-12-10 09:50:06 -0500

    tf 2.4.0

--- main.tf
+++ main.tf
@@ -8,19 +8,28 @@ terraform {
 
 provider "iterative" {}
 
-resource "iterative_task" "tpi-examples-basic" {
-    name      = "tpi-examples-basic "
+resource "iterative_task" "tpi-examples-gpu" {
+    name      = "tpi-examples-gpu"
     cloud     = "aws"
     region    = "us-east-2"
-    machine   = "l+k80"
+    machine   = "m+k80"
     directory = "."
 
     script = <<-END
     #!/bin/bash
     sudo apt update
-    sudo apt-get install -y software-properties-common build-essential python3-pip
-
-    pip3 install -r requirements.txt
-    python3 src/train.py 
+    sudo apt-get install -y software-properties-common build-essential ubuntu-drivers-common
+    sudo ubuntu-drivers autoinstall
+    sudo curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh &&
+    sudo usermod -aG docker ubuntu
+    sudo setfacl --modify user:ubuntu:rw /var/run/docker.sock
+    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+    curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu20.04/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+    sudo apt update && sudo apt install -y nvidia-docker2
+    sudo systemctl restart docker
+    rm get-docker.sh
+    nvidia-smi
+    docker run --rm --gpus all -v "$PWD:/tpi" iterativeai/cml:0-dvc2-base1-gpu \
+        /bin/bash -c "cd /tpi; pip install -r requirements.txt; python src/train.py"
     END
 }
