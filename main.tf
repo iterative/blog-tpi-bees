47b524b29374666e803478c2233b55556fe2fc44
Maria Khalusova <kafooster@gmail.com>
2021-12-10 14:05:29 -0500

    Both basic and gpu examples

--- main.tf
+++ main.tf
@@ -8,6 +8,22 @@ terraform {
 
 provider "iterative" {}
 
+resource "iterative_task" "tpi-examples-basic" {
+    name      = "tpi-examples-basic "
+    cloud     = "aws"
+    region    = "us-east-2"
+    machine   = "l+k80"
+    directory = "."
+
+    script = <<-END
+    #!/bin/bash
+    sudo apt update
+    sudo apt-get install -y software-properties-common build-essential python3-pip
+    pip3 install -r requirements.txt
+    python3 src/train.py 
+    END
+}
+
 resource "iterative_task" "tpi-examples-gpu" {
     name      = "tpi-examples-gpu"
     cloud     = "aws"
