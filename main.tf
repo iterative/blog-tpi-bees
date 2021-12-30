624743c75607d1cc41e058c9bf4348d072b72c8a
MKhalusova <kafooster@gmail.com>
2021-12-30 13:02:26 -0500

    adds simple GPU example with DLAMI

--- main.tf
+++ main.tf
@@ -18,36 +18,24 @@ resource "iterative_task" "tpi-examples-basic" {
     script = <<-END
     #!/bin/bash
     sudo apt update
-    sudo apt-get install -y software-properties-common build-essential python3-pip
+    sudo apt-get install -y python3-pip
     pip3 install -r requirements.txt
     python3 src/train.py 
     END
 }
 
 resource "iterative_task" "tpi-examples-gpu" {
-    name      = "tpi-examples-gpu"
+    name      = "ami-gpu-example"
     cloud     = "aws"
     region    = "us-east-2"
     machine   = "m+k80"
+    disk_size = "130"
+    image     = "ubuntu@898082745236:x86_64:Deep Learning AMI (Ubuntu 18.04) Version 54.0"
     directory = "."
 
     script = <<-END
     #!/bin/bash
-    sudo apt update
-    sudo apt-get install -y software-properties-common build-essential ubuntu-drivers-common
-    sudo ubuntu-drivers autoinstall
-    sudo curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh &&
-    sudo usermod -aG docker ubuntu
-    sudo setfacl --modify user:ubuntu:rw /var/run/docker.sock
-    curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
-    curl -s -L https://nvidia.github.io/nvidia-docker/ubuntu20.04/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-    sudo apt update && sudo apt install -y nvidia-docker2
-    sudo systemctl restart docker
-    rm get-docker.sh
-    
-    nvidia-smi
-
-    docker run --rm --gpus all -v "$PWD:/tpi" iterativeai/cml:0-dvc2-base1-gpu \
-        /bin/bash -c "cd /tpi; pip install -r requirements.txt; python src/train.py"
+    pip3 install -r requirements.txt
+    python3 src/train.py 
     END
 }
