16092dafccf6c8b3b0183056864367098d361574
MKhalusova <kafooster@gmail.com>
2022-02-07 12:21:20 -0500

    updates based on feedback suggestions

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 provider "iterative" {}
 
-resource "iterative_task" "example-gpu" {
+resource "iterative_task" "example-docker-gpu" {
   cloud   = "aws"
   machine = "m+t4"   # 4 CPUs and an NVIDIA Tesla T4 GPU
   spot    = 0
@@ -17,7 +17,7 @@ resource "iterative_task" "example-gpu" {
   environment = { TF_CPP_MIN_LOG_LEVEL = "1" }
   script = <<-END
     #!/bin/bash
-    docker run --rm --gpus all -v "$PWD:/tpi" iterativeai/cml:0-dvc2-base1-gpu \
-      /bin/bash -c "cd /tpi; pip install -r requirements.txt tensorflow==2.8.0; python train.py --output results-gpu/metrics.json"
+    docker run --rm --gpus all -v "$PWD:/tpi" -w /tpi iterativeai/cml:0-dvc2-base1-gpu \
+      /bin/bash -c "pip install -r requirements.txt tensorflow==2.8.0; python train.py --output results-gpu/metrics.json"
   END
 }
