90bf7fb1698e7f01485d86973371aaec27383f94
Casper da Costa-Luis <casper.dcl@physics.org>
2022-05-18 16:43:03 +0100

    forward config var, reduce verbosity

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "iterative_task" "example-docker-gpu" {
   environment = { TF_CPP_MIN_LOG_LEVEL = "1" }
   script = <<-END
     #!/bin/bash
-    docker run --rm --gpus all -v "$PWD:/tpi" -w /tpi iterativeai/cml:0-dvc2-base1-gpu \
+    docker run --rm --gpus all -v "$PWD:/tpi" -w /tpi -e TF_CPP_MIN_LOG_LEVEL iterativeai/cml:0-dvc2-base1-gpu \
       /bin/bash -c "pip install -r requirements.txt tensorflow==2.8.0; python train.py --output results-gpu/metrics.json"
   END
 }
